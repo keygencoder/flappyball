5a5cf1700851c5e0af3e4427b910a5fd720d0ca1
keygen <389053104@qq.com>
2019-08-26 23:56:44 +0800

    'config'

--- flappyball/config.py
+++ flappyball/config.py
@@ -1,7 +1,7 @@
 game_frame = 60
 window_size_x = 320
 window_size_y = 240
-gravity = window_size_y * 4
+gravity = window_size_y * 5
 
 game_background_color = 211, 211, 211
 text_color = 0, 0, 255
@@ -12,6 +12,6 @@ ball_radius = int(window_size_y * 0.025)
 ball_jump_speed = -ball_radius * 55
 
 pipeline_length = ball_radius * 2
-pipeline_free_space_length = ball_radius * 18
-pipeline_distance = ball_radius * 18
-pipeline_horizontal_speed = -ball_radius * 25
+pipeline_free_space_length = ball_radius * 16
+pipeline_distance = ball_radius * 22
+pipeline_horizontal_speed = -ball_radius * 40
